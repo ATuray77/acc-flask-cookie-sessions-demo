c35a21ef142040f3f4fc76c800be1c0bb15ddab8
Aruna Turay <tura_arun@yahoo.com>
2023-09-19 15:37:45 -0400

    abotu to modify and add session info

--- app.py
+++ app.py
@@ -5,15 +5,27 @@ from flask_cors import CORS
 import datetime
 
 app = Flask(__name__)
+app.secret_key = b'\xa7/\xd4\x80p\xd5F\xc1\xa0\x9f\x0cd\xb6\x8f\xa0\xf1' # python -c 'import os; print(os.urandom(16))' 
+
 CORS(app)
 
 @app.route('/')
 def index():
     return '<h1>Cookies and Sessions Demo</h1>'
     
+def expiration_date(delay):
+    expire_date = datetime.datetime.now()
+    expire_date = expire_date + datetime.timedelta(days=delay)
+    return expire_date
 
-# DELIVERABLES
-    # 1. Create a GET '/cookies' custom route
+@app.route("/cookies", methods=['GET'])     # 1. Create a GET '/cookies' custom route
+def cookies():
+    resp = make_response({"message": "Hit cookies route"}, 200)
+    if request.cookies["current_user"]:
+        resp.set_cookie("hello", "world")      # - set a cookie for hello=world
+    else:
+        resp.set_cookie("current_user", "Aruna", expires=expiration_date(90), httponly=True)
+    return resp
     # 2. In route function:
         # - set_trace and inspect request cookies
         # - set a cookie for hello=world
--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,14 @@ function App() {
     - console.log document.cookie
       - Note the cookies that are marked as HTTP only
   */
+ useEffect(() => {
+  fetch("/cookies")
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      console.log(document.cookie)
+    })
+ }, []);
  
   return (
     <div className="App">
